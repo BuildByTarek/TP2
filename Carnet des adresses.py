283bdf5a60134710dd0f12523faa2349ada0836c
Tarek-H <hamrounitarek11@gmail.com>
2026-01-15 20:00:25 -0500

    création de classe SQLitePersonsDatabase pour Carnet des adresses.py

--- Carnet des adresses.py
+++ Carnet des adresses.py
@@ -18,3 +18,66 @@ class BaseDatabase(ABC):
     @abstractmethod
     def selectionner_tout(self):
         pass
+
+class SQLitePersonsDatabase(BaseDatabase):
+
+    def __init__(self, db_name="carnet_adresse.db"):
+        self.db_name = db_name
+
+    def _connexion(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        return conn, cursor
+
+    def creer_table(self):
+        conn, cursor = self._connexion()
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS Persons("
+            "PersonID int, "
+            "Nom varchar(255), "
+            "Prenom varchar(255), "
+            "Mail varchar(255)"
+            ");"
+        )
+        conn.commit()
+        conn.close()
+
+    def inserer_personne(self, pid, nom, prenom, mail):
+        conn, cursor = self._connexion()
+        requete = (
+            "INSERT INTO Persons VALUES ("
+            + str(pid) + ",'"
+            + nom + "','"
+            + prenom + "','"
+            + mail + "');"
+        )
+        cursor.execute(requete)
+        conn.commit()
+        conn.close()
+
+    def modifier_personne(self, pid, nom, prenom, mail):
+        conn, cursor = self._connexion()
+        requete = (
+            "UPDATE Persons SET "
+            "Nom='" + nom + "', "
+            "Prenom='" + prenom + "', "
+            "Mail='" + mail + "' "
+            "WHERE PersonID=" + str(pid) + ";"
+        )
+        cursor.execute(requete)
+        conn.commit()
+        conn.close()
+
+    def supprimer_personne(self, pid):
+        conn, cursor = self._connexion()
+        requete = "DELETE FROM Persons WHERE PersonID=" + str(pid) + ";"
+        cursor.execute(requete)
+        conn.commit()
+        conn.close()
+
+    def selectionner_tout(self):
+        conn, cursor = self._connexion()
+        cursor.execute("SELECT * FROM Persons")
+        resultat = cursor.fetchall()
+        conn.close()
+        return resultat
