1b2d39d5585f405b727d5022b43c7c6cef3cff92
Tarek-H <hamrounitarek11@gmail.com>
2026-01-15 19:47:07 -0500

    création de classe abstraite pour Carnet des adresses.py

--- Carnet des adresses.py
+++ Carnet des adresses.py
@@ -1,3 +1,20 @@
 import sys
 import sqlite3
 from abc import ABC, abstractmethod
+
+class BaseDatabase(ABC):
+    @abstractmethod
+    def creer_table(self):
+        pass
+    @abstractmethod
+    def inserer_personne(self, pid, nom, prenom, mail):
+        pass
+    @abstractmethod
+    def modifier_personne(self, pid, nom, prenom, mail):
+        pass
+    @abstractmethod
+    def supprimer_personne(self, pid):
+        pass
+    @abstractmethod
+    def selectionner_tout(self):
+        pass
